98c7e6ab3a39ae7150a67c558d5cd9329a0668df
AVarshney31 <77407288+AVarshney31@users.noreply.github.com>
2022-07-12 15:30:22 +0530

    Create contactuser.php

---
+++ contactuser.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Contact Us</title> 
+<link rel="stylesheet" type="text/css" href="css/style.css"> 
+<style type="text/css"> 
+#address{ width: 50%;height: 300px;float: left; } 
+#map{ width:50%; height: 300px;float: left; } 
+</style></head><body> 
+<div class="header"> 
+<div class="logo"><h2 align="center">BLOOD BANK MANAGEMENT SYSTEM</h2></div><div class="nav"> 
+<div id="a"><a href="index.php">Home</a></div> 
+<div id="b"><a href="about.php">About Us</a></div> 
+<div id="c"><a href="contact.php">Contact Us</a></div> 
+<div id="d"><a href="login.php">Login</a></div></div> </div> 
+<div class="banner"></div> 
+<div class="container"><br><br><br> 
+<div id="address"> 
+<h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact Blood Bank</h2><br> 
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Address</b>:&nbsp;&nbsp;&nbsp;&nbsp;& nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; BHEL Township,Haridwar,Uttarakhand 249403</p><br> 
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Mobile No.</b> 
+:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;01334-281217</p><br> 
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>E-Mail</b> 
+:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BH ELHospital@gmail.com</p><br></div> <br><br> 
+<div id="map"><center><img src="image/mapp.jpg" width="750px" height="250" ></center></div></div> 
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> <br> 
+<div class="footer"><br><h2 align="center">Copyright@myproject</h2></div><br><br> 
+</body> 
+</html> 
