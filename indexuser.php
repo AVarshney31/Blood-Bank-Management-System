b2d41d51f4071fb1d3ffb4afbc5f5c2664fbf526
AVarshney31 <77407288+AVarshney31@users.noreply.github.com>
2022-07-12 15:28:25 +0530

    Create indexuser.php

---
+++ indexuser.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Blood Bank Home</title> 
+<link rel="stylesheet" type="text/css" href="css/style.css"> 
+</head> 
+<body> 
+<div class="header"> 
+<div class="logo"><h2 align="center">BLOOD BANK MANAGEMENT 
+SYSTEM</h2></div> 
+<div class="nav"> 
+<div id="a"><a href="index.php">Home</a></div> 
+<div id="b"><a href="about.php">About Us</a></div> 
+<div id="c"><a href="contact.php">Contact Us</a></div> 
+<div id="d"><a href="login.php">Login</a></div></div></div> 
+<div class="banner"></div> 
+<div class="container"><br> 
+<h2 align="center" style="color: red">DOCTOR'S INFORMATION</h2><br><br> 
+<center><table border="1px"><tr> 
+<td width="200px" height="50px" style="color: red"><center><b>Doctor's Name</b></center></td> 
+<td width="200px" height="50px" style="color: red"><center><b>Mobile No.</b></center></td> 
+<td width="200px"height="50px"style="color:red"><center><b>Address</b></center></td> 
+<td width="200px" height="50px" style="color: red"><center><b>Specialization</b></center></td></tr><tr> 
+<td width="200px" height="50px"><center>Dr. Amar Lal</center></td> 
+<td width="200px" height="50px"><center>09045125383</center></td> 
+<td width="200px" height="50px"><center>S-281 Main Road,near Sethi Bakers,Shivalik 
+Nagar,Haridwar,Uttarakhand 249403</center></td> 
+<td width="200px" height="50px"><center>Pathologist</center></td></tr> </table></center></div><br><br> 
+<div class="footer"><br><h2 align="center">Copyright@myproject</h2></div><br><br> 
+</body></html> 
